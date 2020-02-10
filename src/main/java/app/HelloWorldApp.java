0e8c7fd70584de82ca7c6d997e05a6daa50c6a30
mirzayev.mirzayev <mirzayev.mirzayev@gmail.com>
2020-02-10 18:47:43 +0400

    #1

--- src/main/java/app/HelloWorldApp.java
+++ src/main/java/app/HelloWorldApp.java
@@ -1,9 +1,19 @@
 package app;
 
+import java.util.Scanner;
+
 public class HelloWorldApp {
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
+      Scanner in=new Scanner(System.in);
+      System.out.println("Input first number:");
+      int a=in.nextInt();
+      System.out.println("Input second number:");
+      int b=in.nextInt();
+      int sum=a+b;
+      System.out.println("The sum of your numbers: "+sum);
+
+
   }
 
 }
