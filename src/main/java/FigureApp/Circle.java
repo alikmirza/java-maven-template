9f022f5302bac69dc8a1fa43d8b840410479b83c
mirzayev.mirzayev <mirzayev.mirzayev@gmail.com>
2020-02-21 19:47:26 +0400

    #1

---
+++ src/main/java/FigureApp/Circle.java
@@ -0,0 +1,25 @@
+package FigureApp;
+
+public class Circle extends Figure {
+    Point center;
+    int radius;
+
+    public Circle(Point center, int radius) {
+        this.center=center;
+        this.radius=radius;
+    }
+
+    public static Circle rand() {
+        return new Circle(Point.rand(), Fn.rand());
+    }
+
+    @Override
+    public int area() {
+        return (int)(Math.PI * radius * radius);
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Area of Circle: %d",area());
+    }
+}
---
+++ src/main/java/FigureApp/Fn.java
@@ -0,0 +1,7 @@
+package FigureApp;
+
+public class Fn {
+    public static int rand() {
+        return (int)(Math.random()*20+5);
+    }
+}
