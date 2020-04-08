00f01f0fd728a17f18050923167c32e669b69eb3
mirzayev.mirzayev <mirzayev.mirzayev@gmail.com>
2020-04-08 18:33:24 +0400

    #Updated

--- src/main/java/lesson29/StringOp.java
+++ src/main/java/lesson29/StringOp.java
@@ -2,27 +2,24 @@ package lesson29;
 
 public class StringOp {
     public static String shift(String origin, int toLeft, int toRight){
-        String ex;
-        if (toLeft != 0)
-        ex=left(origin,toLeft);
-        else
-        ex=right(origin,toRight);
-        return ex;
+        String first;
+        String updated;
+        first=left(origin,toLeft);
+        updated=right(first,toRight);
+        return updated;
     }
 
-    static String left(String origin, int left)
-    {
+    static String left(String origin, int left) {
         return origin.substring(left) + origin.substring(0, left);
     }
-    static String right(String origin, int right)
-    {
+    static String right(String origin, int right) {
         return left(origin, origin.length() - right);
     }
 
     public static void main(String[] args) {
         StringOp stringOp=new StringOp();
         String ex="Hello";
-        System.out.println(stringOp.shift(ex,5,5));
+        System.out.println(stringOp.shift(ex,1,1));
 
     }
 
